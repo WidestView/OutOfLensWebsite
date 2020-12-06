d5f0864e0e9cc9b4c0c8d8b216ece51c2412a479
figurantpp <renan.r.marcelino2@protonmail.com>
2020-12-06 15:30:57 -0300

    Added Initial Views

--- OutOfLensWebsite/Controllers/AdminController.cs
+++ OutOfLensWebsite/Controllers/AdminController.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using Microsoft.AspNetCore.Mvc;
-using OutOfLens_ASP.Models;
 using OutOfLensWebsite.Models;
 
 namespace OutOfLensWebsite.Controllers
@@ -13,13 +12,19 @@ namespace OutOfLensWebsite.Controllers
             return View();
         }
 
-        public IActionResult Insert(string form)
+
+        public IActionResult Add(string form)
         {
-            string[] validForms = {"insertion/employee"};
+            string[] validForms = {"insertion/customer"};
+
+            if (string.IsNullOrEmpty(form))
+            {
+                form = "customer";
+            }
 
             string path = "insertion/" + form;
 
-            if (!validForms.Contains(path))
+            if (validForms.Contains(path))
             {
                 return View(path);
             }
@@ -44,17 +49,37 @@ namespace OutOfLensWebsite.Controllers
                     employee.Register(database);
                 }
 
-                return View("Insertion/Employee");
+                return View("Index");
             }
         }
 
         public IActionResult Query(string table)
         {
-            string[] validTables = {"query/employee"};
+            string[] validTables = {"query/employee", "query/customer"};
+
+            if (string.IsNullOrEmpty(table))
+            {
+                table = "employee";
+            }
 
             string path = "query/" + table;
 
             return validTables.Contains(path) ? (IActionResult) View(path) : NotFound();
+        }
+
+        public IActionResult Operation()
+        {
+            return View();
+        }
+
+        public IActionResult Report()
+        {
+            return View();
+        }
+
+        public IActionResult Agenda()
+        {
+            return View();
         }
     }
 }
